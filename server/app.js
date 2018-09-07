9b44386a2ca9b8f4242da09cffc577820fa4684a
Den Temple <developer@dentemple.com>
2018-09-07 13:42:57 -0400

    Configure the basic generator

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const app = express();
 
 // Global Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '..', 'public'))); // if we don't have any files, we can run this
 
